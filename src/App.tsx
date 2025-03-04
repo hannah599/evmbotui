a24cc77881f1df9fb050d2e03a9fb18a4e9c89b7
hananh <afq33owri5z@hotmail.com>
2025-03-04 21:21:43 +0800

    react+ts first version

---
+++ src/App.tsx
@@ -0,0 +1,24 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <div>
+        <h1>EVMBot</h1>
+        <div className="card">
+          <button onClick={() => setCount((count) => count + 1)}>
+            count is {count}
+          </button>
+          <p>
+            Welcome to EVMBot - your Ethereum Virtual Machine assistant!
+          </p>
+        </div>
+      </div>
+    </>
+  )
+}
+
+export default App
